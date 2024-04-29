27c2c698c802170448a5e17a5d5aa7dfbd118fac
akumar23 <aryan.kumar@nalej.com>
2024-04-29 10:08:42 -0700

    updated text on graph

--- neural_net.py
+++ neural_net.py
@@ -10,10 +10,9 @@ def test_prediction(X_train, Y_train, index, W1, b1, W2, b2):
     current_image = X_train[:, index, None]
     prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
     label = Y_train[index]
-    print("Prediction: ", prediction)
-    print("Label: ", label)
     
     current_image = current_image.reshape((28, 28)) * 255
     plt.gray()
     plt.imshow(current_image, interpolation='nearest')
+    plt.text(-4.5, -1.5, "Prediction: "+str(prediction)+"\n"+"Label: "+str(label), bbox=dict(facecolor='red', alpha=0.5), fontsize=12)
     plt.show()
